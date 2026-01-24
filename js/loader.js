f79acc2ea0d1371e097dc885382372fbd1589940
Odongo Lovemore <lovemoreodongo02@gmail.com>
2026-01-24 12:31:10 +0300

    Removed esLinters, prettier and husky which were failing to allow me push my work

---
+++ js/loader.js
@@ -0,0 +1,9 @@
+function loadPage(page) {
+  fetch(page)
+    .then(response => response.text())
+    .then(html => {
+      document.getElementById('mainContent').innerHTML = html;
+    })
+    .catch(err => console.error('Error loading page:', err));
+}
+loadPage();
