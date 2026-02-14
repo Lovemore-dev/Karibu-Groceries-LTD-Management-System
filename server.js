399e7d8ce874a82ae627b4c1fc8a5aa083c805f7
Odongo Lovemore <lovemoreodongo02@gmail.com>
2026-02-14 21:00:14 +0300

    added a comment to intentionally ignore the eslint error on the log meant for development

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+// load environment variables from .env file
+require('dotenv').config();
+// import express
 const express = require('express');
-const dotenv = require("dotenv").config()
+// import database connection
+const connectDB = require('./config/db');
+// initialize express
+
 const app = express();
+
+connectDB();
+app.use(express.json());
 const port = process.env.PORT;
 
-app.use("/api/procurements",require("./routes/procurementRoute.js"))
+app.use('/api/procurements', require('./routes/procurementRoute'));
 
-app.listen(port,()=>{
-    console.log(`The server is running on port ${port}`);
-    
-})
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`The server is running on port ${port}`);
+});
