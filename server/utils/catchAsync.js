d8b01a1b12229833fc46d01a7fc7768acbe3c0e9
Odongo Lovemore <lovemoreodongo02@gmail.com>
2026-03-07 13:43:16 +0300

    modified utils to debug an error with functions

--- server/utils/catchAsync.js
+++ server/utils/catchAsync.js
@@ -1,5 +1,12 @@
 module.exports = (fn) => {
   return (req, res, next) => {
-    fn(req, res, next).catch(next);
+    const safeNext =
+      typeof next === 'function'
+        ? next
+        : (err) => {
+            throw err;
+          };
+
+    return Promise.resolve(fn(req, res, safeNext)).catch(safeNext);
   };
 };
